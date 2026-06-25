5824b0342d9bf683bb386c83e4d02f75c4054bcc
jbarcellos832-criador <jbarcellos832@gmail.com>
2026-06-24 23:03:10 -0300

    Update webhook-ironpay.js

--- netlify/functions/webhook-ironpay.js
+++ netlify/functions/webhook-ironpay.js
@@ -23,7 +23,7 @@ exports.handler = async (event) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        from: "suporte@slothia.space",
+       from: "Slothia <onboarding@resend.dev>",
         to: emailCliente,
         subject: "Seu acesso à Slothia",
         html: "<h2>Ola! Seu pagamento foi confirmado!</h2><p>Aqui estao seus dados de acesso:</p><p><strong>Link:</strong> https://slothia.space/cliente</p><p><strong>Senha:</strong> cBCCIRQSUk2rJNAe</p><p>Qualquer duvida, responda este email.</p><p>Equipe Slothia</p>"
